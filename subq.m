2360c7248f306a69247f01f94b4ae801b9d5010b
CSSticktar <44072364+charleslstewart1@users.noreply.github.com>
2018-10-14 18:51:27 -0500

    Random Number Subtraction
    Random PreK level subtraction generator, never neg.

---
+++ subq.m
@@ -0,0 +1,20 @@
+function [block]=subq()
+clc,clear
+
+randnum1 = randi(10);
+randnum2 = randi ([0,randnum1]);
+math = randnum1 - randnum2;
+
+addq = input (['What is ' num2str(randnum1) ' - ' num2str(randnum2) ' ?  ']);
+
+        if addq == math
+            block=1;
+            disp ('Good Job') %Has to be in order, once it finds one it will stop running
+        elseif addq > math
+            block=0;
+            disp ('To High')
+        else
+            block=0;
+            disp('To Low')
+            
+        end
