128693a43830bd001b0266096c1ed2f59f50c8cf
CSSticktar <44072364+charleslstewart1@users.noreply.github.com>
2018-10-14 18:54:35 -0500

    Random Addition Gen
    PreK level addition problems, randomly generates.  answer never exceeds 10.

---
+++ addq.m
@@ -0,0 +1,18 @@
+function [block]= addq()
+randnum1 = randi(5);
+randnum2 = randi (5);
+math = randnum1 + randnum2;
+
+addq = input (['What is ' num2str(randnum1) ' + ' num2str(randnum2) ' ?  ']);
+
+        if addq == math
+            block=1;
+            disp ('Good Job') %Has to be in order, once it finds one it will stop running
+        elseif addq > math
+            block=0;
+            disp ('To High')
+        else
+            block=0;
+            disp('To Low')
+            
+        end
