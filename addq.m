7e72c8844c3cba716be48018a0b8f9d2a22f9fa1
CSSticktar <44072364+charleslstewart1@users.noreply.github.com>
2018-10-14 18:58:44 -0500

    Addition Function

--- addq.m
+++ addq.m
@@ -7,7 +7,7 @@ addq = input (['What is ' num2str(randnum1) ' + ' num2str(randnum2) ' ?  ']);
 
         if addq == math
             block=1;
-            disp ('Good Job') %Has to be in order, once it finds one it will stop running
+            disp ('Good Job') %Has to be in order, once it finds one it will stop running.
         elseif addq > math
             block=0;
             disp ('To High')
