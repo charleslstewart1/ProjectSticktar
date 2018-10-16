ff7f1b4e413678fd88df59b561c5dde83a5b511c
CSSticktar <44072364+charleslstewart1@users.noreply.github.com>
2018-10-15 20:25:07 -0500

    Added "Hint" Output
    creates a logic statement and a cell array with "Hint" string.  Don't know how to display in GUI

--- addq.m
+++ addq.m
@@ -1,4 +1,4 @@
-function [block]= addq()
+function [block, hint]= addq()
 randnum1 = randi(5);
 randnum2 = randi (5);
 math = randnum1 + randnum2;
@@ -7,12 +7,12 @@ addq = input (['What is ' num2str(randnum1) ' + ' num2str(randnum2) ' ?  ']);
 
         if addq == math
             block=1;
-            disp ('Good Job') %Has to be in order, once it finds one it will stop running.
+            hint = {'Good Job'} %Has to be in order, once it finds one it will stop running
         elseif addq > math
             block=0;
-            disp ('To High')
+            hint = {'To High'}
         else
             block=0;
-            disp('To Low')
+            hint = {'To Low'}
             
         end
